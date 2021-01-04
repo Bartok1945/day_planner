d87f5eed7f66c0ad5fad757343aa02394fae248b
Bartok <thom.huenger@gmail.com>
2021-01-04 12:37:57 -0500

    refactoring

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
-//variables
+//global variables
 
-var currentDate
-var currentHour
+//wrap with document.ready function
 
 
-    $(document).ready(function() {
-        var interval = setInterval(function() {
-            var momentNow = moment();
-            $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
-                                + momentNow.format('dddd')
-                                 .substring(0,3).toUpperCase());
-            $('#time-part').html(momentNow.format('A hh:mm:ss'));
-        }, 100);
-    });
+//function create hour blocks
+//create row
+//create columns- 3 columns, time, task, save
+//add them to the page
 
-//functions
+//event listener for save button--function save to local storage
+//function write from local storage
 
-//localStorage
+//if else statements for changing colors as time marches on
 
+//wrap with document.ready function
+$(document).ready(function () {
+    //display date using moment()
+    var now = moment();
+    var currentDate = now.format("MM DD YYYY");
+    $("#currentDay").text("Today's Date: " + currentDate);
+
+    //function to create rows and columns
+
+    for (var i = 0; i < 9; i++) {
+        var rowBlock = $("<div>").addClass("row");
+        var timeBlock = $("<div>").addClass("time-block col-md-2");
+        var taskBlock = $("<textarea>").addClass("col-md-9");
+        var saveButton = $("<button>").addClass("saveBtn col-md-1").html('<i class="fas fa-save"></i>');
+        //add elements to the DOM
+        $(".container").append(rowBlock);
+        $(rowBlock).append(timeBlock);
+        $(timeBlock).after(taskBlock);
+        $(taskBlock).after(saveButton);
+
+        //print the time on each timeblock
+        $(timeBlock).text(moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
+        timeBlock.attr('data-time', moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
+
+        //color coding statement
+        if (now.isSame(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
+            $(taskBlock).addClass('present');
+
+        } else if (now.isBefore(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
+            $(taskBlock).addClass('future');
+
+        } else if (now.isAfter(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
+            $(taskBlock).addClass('past');
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+});
