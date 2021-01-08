7c09d3f64c5ee6ccd6eb3c0cac8e6809fbce57b9
Bartok <thom.huenger@gmail.com>
2021-01-08 10:58:45 -0500

    readme and png file

---
+++ README.md
@@ -0,0 +1,12 @@
+#Day Planner
+
+*This is a simple day planner app that uses jQuery to build all elements and functionality.
+
+*The app is designed to save information to local storage and appear on the page after refresh and disappear with each new day.
+
+*This app uses moment() to display and track date and time
+
+*Color coded and designed to change color through out the day as time passes
+
+*CSS bootstrap
+
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
-       
+
 
     // display date using moment()
     var now = moment();
@@ -17,14 +17,13 @@ $(document).ready(function () {
         $(timeBlock).text(moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
         var timeHour = (moment('9:00 AM', 'hh:mm A').add(i, 'hours').format("hA"));
         timeBlock.attr('id', timeHour);
+        
         //add elements to the DOM
         $(".container").append(rowBlock);
         $(rowBlock).append(timeBlock);
         $(timeBlock).after(taskBlock);
         $(taskBlock).after(saveButton);
-        //print the time on each timeblock
-        // $(timeBlock).text(moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
-        // timeBlock.attr('id', + moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
+   
         //color coding statement
         if (now.isSame(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
             $(taskBlock).addClass('present');
@@ -37,28 +36,31 @@ $(document).ready(function () {
         }
     }
     // save event to local storage
-    $(".saveBtn").on("click", function() {
+    $(".saveBtn").on("click", function () {
         var hourlyArray = JSON.parse(localStorage.getItem("task")) || [];
-            for (var i = 0; i < hourlyArray.length; i++) {
-                if (hourlyArray[i].hour===$(this).siblings("div.time-block").attr("id")) {
-                    hourlyArray[i].event=$(this).prev().val()
-                    //only if it wasn't found, push it, variable true/false
-                }
-            }
-            
+        for (var i = 0; i < hourlyArray.length; i++) {
+
+            //**TO BE FINISHED** statement to clear storage when new tasks are added
+
+            // if (hourlyArray[i].hour===$(this).siblings("div.time-block").attr("id")) {
+            //     hourlyArray[i].event=$(this).prev().val()
+            //     //only if it wasn't found, push it, variable true/false
+            // }
+        }
+
         // console.log($(this))
         hourlyArray.push({
             event: $(this).prev().val(),
             hour: $(this).siblings("div.time-block").attr("id"),
         })
-        localStorage.setItem("task", JSON.stringify(hourlyArray));        
+        localStorage.setItem("task", JSON.stringify(hourlyArray));
     });
 
     if (localStorage.getItem("task") !== null) {
         hourlyArray = JSON.parse(localStorage.getItem("task"));
-        hourlyArray.forEach(function(task){
-        $("#" + task.hour)[0].nextSibling.value=(task.event);
-           
+        hourlyArray.forEach(function (task) {
+            $("#" + task.hour)[0].nextSibling.value = (task.event);
+
         })
     };
 
