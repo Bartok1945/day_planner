2003ec8cb268c1e2c86e8a4ca3427937c3e1617e
Bartok <thom.huenger@gmail.com>
2021-01-08 10:35:21 -0500

    fixing local storage

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-//global variables
 
-//wrap with document.ready function
-
-
-//function create hour blocks
-//create row
-//create columns- 3 columns, time, task, save
-//add them to the page
-
-//event listener for save button--function save to local storage
-//function write from local storage
-
-//if else statements for changing colors as time marches on
-
-//wrap with document.ready function
 $(document).ready(function () {
-    //display date using moment()
+       
+
+    // display date using moment()
     var now = moment();
     var currentDate = now.format("MM DD YYYY");
     $("#currentDay").text("Today's Date: " + currentDate);
@@ -25,18 +12,19 @@ $(document).ready(function () {
     for (var i = 0; i < 9; i++) {
         var rowBlock = $("<div>").addClass("row");
         var timeBlock = $("<div>").addClass("time-block col-md-2");
-        var taskBlock = $("<textarea>").addClass("col-md-9");
+        var taskBlock = $("<textarea>").addClass("schedule-item col-md-9");
         var saveButton = $("<button>").addClass("saveBtn col-md-1").html('<i class="fas fa-save"></i>');
+        $(timeBlock).text(moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
+        var timeHour = (moment('9:00 AM', 'hh:mm A').add(i, 'hours').format("hA"));
+        timeBlock.attr('id', timeHour);
         //add elements to the DOM
         $(".container").append(rowBlock);
         $(rowBlock).append(timeBlock);
         $(timeBlock).after(taskBlock);
         $(taskBlock).after(saveButton);
-
         //print the time on each timeblock
-        $(timeBlock).text(moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
-        timeBlock.attr('data-time', moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
-
+        // $(timeBlock).text(moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
+        // timeBlock.attr('id', + moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
         //color coding statement
         if (now.isSame(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
             $(taskBlock).addClass('present');
@@ -47,15 +35,32 @@ $(document).ready(function () {
         } else if (now.isAfter(moment('9:00 AM', 'hh:mm A').add(i, 'hours'), 'hour')) {
             $(taskBlock).addClass('past');
         }
-
     }
+    // save event to local storage
+    $(".saveBtn").on("click", function() {
+        var hourlyArray = JSON.parse(localStorage.getItem("task")) || [];
+            for (var i = 0; i < hourlyArray.length; i++) {
+                if (hourlyArray[i].hour===$(this).siblings("div.time-block").attr("id")) {
+                    hourlyArray[i].event=$(this).prev().val()
+                    //only if it wasn't found, push it, variable true/false
+                }
+            }
+            
+        // console.log($(this))
+        hourlyArray.push({
+            event: $(this).prev().val(),
+            hour: $(this).siblings("div.time-block").attr("id"),
+        })
+        localStorage.setItem("task", JSON.stringify(hourlyArray));        
+    });
 
-
-
-
-
-
-
+    if (localStorage.getItem("task") !== null) {
+        hourlyArray = JSON.parse(localStorage.getItem("task"));
+        hourlyArray.forEach(function(task){
+        $("#" + task.hour)[0].nextSibling.value=(task.event);
+           
+        })
+    };
 
 
 });
